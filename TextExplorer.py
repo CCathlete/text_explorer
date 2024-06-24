8bc7b4fb7eddb0635621947ca42ff922a44e738c
CCat <ccathlete01@gmail.com>
2024-06-24 18:36:08 +0200

    Trying the bio roberta model for document qa, after serialising the result.

--- TextExplorer.py
+++ TextExplorer.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from transformers import pipeline
 from LocalParser.Parser import Parser
+import numpy as np
 
 app = Flask(__name__)
 myParser = Parser()
@@ -20,6 +21,7 @@ def askQuestion():
     # Getting answers from the modules.
     print("\nAsking bio roberta your question.\n")
     bioRobertaAnswer = bioRobertaQA(question=question, context=context)
+    bioRobertaAnswerSerialisable = {k: makeSerialiseable(v) for k,v in bioRobertaAnswer.items()}
     # bertAnswer = bertQa(question=question, context=context)
     # robertaAnswer = robertaQa(question=question, context=context)
     # t5Answer = t5Qa(question=question, context=context)
@@ -27,13 +29,33 @@ def askQuestion():
     # Combining the answers. TODO: choose the best one with some logic.
     print("\nWe have an answer:\n")
     combinedAnswers = {
-        'bioRoberta': bioRobertaQA,
+        'bioRoberta': bioRobertaAnswerSerialisable,
         # 'bert': bertAnswer,
         # 'roberta': robertaAnswer,
         # 't5': t5Answer
     }
 
     return jsonify(combinedAnswers)
+
+def makeSerialiseable(obj):
+    """Gets an unknown object and makes sure it can be printed in a JSON.
+
+    Args:
+        obj (any): the output of the ai model.
+
+    Returns:
+        dict|list|str|int|float|bool: an object witih a printable type.
+    """
+    if isinstance(obj, np.ndarray):
+        return obj.tolist() # Converting a numpy array to list.
+    elif isinstance(obj,(np.int64, np.int32)):
+        return int(obj) # Convertinr numpy ints to standard Python int.
+    elif isinstance(obj, (np.float64, np.float32)):
+        return float(obj) # Convertinr numpy floats to standard Python float.
+    elif isinstance(obj, (dict, list, str, int, float, bool, type(None))):
+        return obj # If it's already serialisable we just return it.
+    else:
+        return str(obj) # A fallback in case it's a custom object.
     
 
     
